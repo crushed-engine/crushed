8c0c7a568517d685fdb0fb462126bee65232f1cf
manen <varga.bercel.i@gmail.com>
2021-03-03 14:26:25 +0100

    Win test

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.15
 
 require (
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20201108214237-06ea97f0c265
-	github.com/manen/cleaner v0.0.0-20210302132856-e6d5ff4cc41e
+	github.com/manen/cleaner v0.0.0-20210302134558-b1587b2d35de
 )
--- render/win/win_test.go
+++ render/win/win_test.go
@@ -4,12 +4,15 @@ import (
 	"testing"
 
 	"github.com/crushed/crushed/render/win"
+	"github.com/manen/cleaner"
 )
 
 func TestWin(t *testing.T) {
-	w := win.NewWindow(640, 480, "Test Window")
+	w := win.NewWindow(640, 480, "Test Window ")
 
 	for !w.ShouldClose() {
 		win.PollEvents()
 	}
+
+	cleaner.CleanUp()
 }
